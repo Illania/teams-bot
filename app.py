4edf7211d74638710ebdc66edd41ecfcbf591df9
Anna <anna_zl@rambler.ru>
2024-02-17 02:05:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ CONFIG = DefaultConfig()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID)
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
 
 
